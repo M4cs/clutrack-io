23b6fdab34228e1a55a01d4e1323c56088724ce8
Max <max@Maxs-MacBook-Pro.local>
2021-05-23 16:40:57 -0400

    Fix Reload

--- app/templates/assets/app.js
+++ app/templates/assets/app.js
@@ -41,7 +41,7 @@ function handleAuth(accountAddress, signature, message_hash) {
         })
     }).then((response) => {
         response.json().then(data => {
-            window.location = data.redirect;
+            window.location.reload();
         });
     });
 }
